d7b832395b9a9b03c95516ff89f16ecb11907ec2
Mateusz Bąk <bakmatthew@icloud.com>
2025-06-17 23:25:06 +0200

    Test

--- swift-macro-compatibility-check.sh
+++ swift-macro-compatibility-check.sh
@@ -131,6 +131,8 @@ for version in "${VERSIONS[@]}"; do
   else
     echo -e "${RED}Compatibility check failed for swift-syntax $version${NC}"
   fi
+  
+  swift package clean
 done
 
 # Summary of results
